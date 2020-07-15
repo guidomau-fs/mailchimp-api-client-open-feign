371a6560cb874f5623b8f8d4f28c12bb7bba0cdd
Guido Mauricio Mantilla T <gmantilla@fastspring.com>
2020-07-15 07:20:37 -0500

    GitHub Sample Commit

--- src/main/java/sample/github/Contributor.java
+++ src/main/java/sample/github/Contributor.java
@@ -3,4 +3,20 @@ package sample.github;
 public class Contributor {
     String login;
     int contributions;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getContributions() {
+        return contributions;
+    }
+
+    public void setContributions(int contributions) {
+        this.contributions = contributions;
+    }
 }
