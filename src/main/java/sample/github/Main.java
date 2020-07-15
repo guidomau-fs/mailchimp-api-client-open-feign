ca221ad33407ce5113ad2721d5ef722f7668ab78
Guido Mauricio Mantilla T <gmantilla@fastspring.com>
2020-07-15 08:10:52 -0500

    GitHub Sample Commit

--- src/main/java/sample/github/Main.java
+++ src/main/java/sample/github/Main.java
@@ -2,8 +2,12 @@ package sample.github;
 
 import feign.Feign;
 import feign.Logger.Level;
+import feign.Retryer;
+import feign.codec.ErrorDecoder;
+import feign.http2client.Http2Client;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.micrometer.MicrometerCapability;
 import feign.slf4j.Slf4jLogger;
 
 import java.util.List;
@@ -13,9 +17,13 @@ public class Main {
     public static void main(String[] args) {
 
         GitHub github = Feign.builder()
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .logger(new Slf4jLogger())
+                .client(new Http2Client())                  // Under the hood http client
+                .encoder(new JacksonEncoder())              // Under the hood JSON Encoder
+                .decoder(new JacksonDecoder())              // Under the hood JSON Decoder
+                .errorDecoder(new ErrorDecoder.Default())   // Custom JSON Decoder Error Handling
+                .retryer(new Retryer.Default())             // Custom Retry logic
+                .addCapability(new MicrometerCapability())  // Metrics data collection
+                .logger(new Slf4jLogger())                  // Define Slf4j as a default logger
                 .logLevel(Level.FULL)
                 .target(GitHub.class, "https://api.github.com");
 
