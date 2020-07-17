9399dfafe355b11607c5a7d78f875f66af4c6677
Guido Mauricio Mantilla T <gmantilla@fastspring.com>
2020-07-17 09:13:29 -0500

    New Key Commit

--- src/main/java/com/brightmarket/mailchimp/api/factory/ApiKeyFactory.java
+++ src/main/java/com/brightmarket/mailchimp/api/factory/ApiKeyFactory.java
@@ -3,6 +3,6 @@ package com.brightmarket.mailchimp.api.factory;
 public class ApiKeyFactory {
 
     public static String retrieveApiKey(){
-        return "apikey a5c1547d8b131702938b148efd8b47ff-us10";
+        return "apikey 2ad1b0096026b3ff479483be8981e5fb-us10";
     }
 }
