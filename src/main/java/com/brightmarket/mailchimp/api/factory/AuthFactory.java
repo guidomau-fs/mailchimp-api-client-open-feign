d62ef64f5df7239f7b911e04ad4817478e87ee40
Guido Mauricio Mantilla T <gmantilla@fastspring.com>
2020-07-23 11:38:23 -0500

    Refactor Commit

--- src/main/java/com/brightmarket/mailchimp/api/factory/AuthFactory.java
+++ src/main/java/com/brightmarket/mailchimp/api/factory/AuthFactory.java
@@ -3,6 +3,6 @@ package com.brightmarket.mailchimp.api.factory;
 public class AuthFactory {
 
     public static String retrieveAuth(){
-        return "OAuth ";
+        return "OAuth ffd38645c46baeb08ad4b55791662219-dummy";
     }
 }
