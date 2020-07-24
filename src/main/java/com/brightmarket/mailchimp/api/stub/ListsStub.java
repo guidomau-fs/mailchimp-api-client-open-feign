ccc9e9c162f2a2488ff47ef8dced859265890ed2
Guido Mauricio Mantilla T <gmantilla@fastspring.com>
2020-07-24 09:49:42 -0500

    Refactor Commit

--- src/main/java/com/brightmarket/mailchimp/api/stub/ListsStub.java
+++ src/main/java/com/brightmarket/mailchimp/api/stub/ListsStub.java
@@ -8,6 +8,8 @@ import feign.RequestLine;
 
 public interface ListsStub {
 
+    // ----- Lists ----- //
+
     @Headers({
             "Authorization: {token}"
     })
@@ -31,4 +33,13 @@ public interface ListsStub {
     })
     @RequestLine("DELETE /lists/{list_id}")
     void deleteList(@Param("token") String token, @Param("list_id") String listId);
+
+    @Headers({
+            "Authorization: {token}"
+    })
+    @RequestLine("PATCH /lists/{list_id}")
+    List updateList(@Param("token") String token, @Param("list_id") String listId, List list);
+
+
+    // ----- Members Lists ----- //
 }
